f27930859338600b75992688db4df77e46d7c2ca
Bubashankushan <91317344+L00163287@users.noreply.github.com>
2021-10-04 22:39:43 +0100

    Initialize question 01

---
+++ Questions/Question01.py
@@ -0,0 +1,11 @@
+"""
+#
+# @File         : Question01.py
+# @Created      : 2021-10-04 22:37
+# @Author       : Bubashankushan B
+# @Version      : v1.0.0
+# @Licensing    : 
+#
+# @Description  :
+#
+"""
